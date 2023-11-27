c6d9f81099eb5599b12566f8cd224dac4cb929f3
deoresulakshana <deoresulakshana@gmail.com>
2023-11-27 09:44:03 +0530

    Add project files.

---
+++ WebApiDemo2/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebApiDemo2.Models;
+using System.Data.SqlClient;
+
+namespace WebApiDemo2.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public List<ProductModel> GetProducts()
+        {
+            SqlConnection cn = new SqlConnection("server=Sulakshana\\sqlexpress;Integrated Security=true;database=Northwind");
+            SqlCommand cmd = new SqlCommand("select * from  products",cn);
+            cn.Open();
+            SqlDataReader dr=cmd.ExecuteReader();
+            List<ProductModel> products = new List<ProductModel>();
+            while (dr.HasRows)
+            {
+                ProductModel productModel = new ProductModel();
+                productModel.Prodid = Convert.ToInt32(dr["0"]); 
+                productModel.Prodname = dr["1"].ToString();
+                productModel.Price = Convert.ToInt32(dr["UnitPrice"]);
+                productModel.Qty = Convert.ToInt32(dr["QuantityInHand"]);
+                products.Add(productModel); 
+
+
+            }
+            cn.Close();
+            return products;    
+        }
+        //public ProductModel GetProduct(int id) {
+        
+        //}
+
+        public HttpStatusCode PostProduct(ProductModel product) {
+            SqlConnection cn = new SqlConnection("server=Sulakshana\\sqlexpress;Integrated Security=true;database=Northwind");
+            SqlCommand cmd = new SqlCommand("insert into product(productname,UnitPrice,QuantityInHand) values (@productname,@UnitPrice,@QuantityInHand)", cn);
+            cmd.Parameters.AddWithValue("@productname", product.Prodname);
+            cmd.Parameters.AddWithValue("@UnitPrice", product.Price);
+            cmd.Parameters.AddWithValue("@QuantityInHand", product.Qty);
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+
+            return HttpStatusCode.OK;
+
+
+
+        }
+        //public void DeleteProduct(int id) { }   
+        //public HttpStatusCode PutProduct(ProductModel product) { }
+
+
+
+    }
+}
---
+++ WebApiDemo2/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDemo2.Models
+{
+    public class ProductModel
+    {
+        public int Prodid { get; set; }
+
+        public string Prodname { get; set; }
+        public int Price { get; set; }
+       public int Qty { get; set; }
+    }
+}
